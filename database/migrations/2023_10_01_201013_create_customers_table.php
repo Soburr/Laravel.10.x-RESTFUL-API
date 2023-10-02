6a086615a0b612b95bd8f6094aa3e2ef7c0a4c32
Soburr <adebesinbenzenejr99@gmail.com>
2023-10-02 17:41:53 +0100

    feature/seeded-migrations

--- database/migrations/2023_10_01_201013_create_customers_table.php
+++ database/migrations/2023_10_01_201013_create_customers_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('address');
             $table->string('city');
+            $table->string('state');
             $table->string('postal_code');
             $table->timestamps();
         });
