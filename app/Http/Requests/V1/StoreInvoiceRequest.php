73de984ca12577836074b119f18de78c04c3e448
Soburr <adebesinbenzenejr99@gmail.com>
2023-10-05 18:02:03 +0100

    feature/creating-resources

--- app/Http/Requests/V1/StoreInvoiceRequest.php
+++ app/Http/Requests/V1/StoreInvoiceRequest.php
@@ -1,17 +1,17 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\V1;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreCustomerRequest extends FormRequest
+class StoreInvoiceRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
