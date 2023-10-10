bde39cafd1564393468e3eb6a1a31ce6b65d2c67
Soburr <adebesinbenzenejr99@gmail.com>
2023-10-10 07:12:11 +0100

    feature

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,6 +29,37 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/setup', function() {
+     $credentials = [
+        'email' => 'admin@admin.com',
+        'password' => 'password'
+     ];
+
+     if(!Auth::attempt($credentials)) {
+        $user = new User;
+
+        $user->name = 'Admin';
+        $user->email = $credentials['email'];
+        $user->password = Hash::make($credentials['email']);
+
+        $user->save();
+
+     if(Auth::attempt($credentials)) {
+        $user = Auth::user();
+
+        $adminToken = $user->createToken('admin-token', ['create', 'update', 'delete']);
+        $updateToken = $user->createToken('update-token', ['create', 'update']);
+        $basicToken = $user->createToken('basic-token');
+
+        return [
+            'admin' => $adminToken->plainTextToken,
+            'update' => $updateToken->plainTextToken,
+            'basic' => $basicToken->plainTextToken,
+        ];
+     }
+     }
 });
 
 require __DIR__.'/auth.php';
